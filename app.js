77402375cabf6baee1270a321d867fe9b8d8f131
Sam Helman <sam.d.helman@gmail.com>
2015-07-14 22:24:43 -0400

    Fixed step 6

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = angular.module('tutorial', []);
+app.controller('PlaylistCtrl', PlaylistCtrl);
+function PlaylistCtrl($scope) {
+    $scope.currentSong = ''; 
+    $scope.playlist = [];
+    $scope.currentName = '';
+    $scope.savedPlaylists = [];
+    $scope.currentPlaylist = {};
+
+    $scope.addSong = function() {
+        $scope.playlist.push($scope.currentSong);  
+        $scope.currentSong = '';
+    };
+
+    $scope.removeSong = function(song) {
+        $scope.playlist.splice($scope.playlist.indexOf(song), 1);
+    };
+
+    $scope.savePlaylist = function() {
+        $scope.savedPlaylists.push({
+            name: $scope.currentName,
+            playlist: $scope.playlist
+        });
+        $scope.currentSong = '';
+        $scope.playlist = [];
+        $scope.currentName = '';
+    };
+
+    $scope.setCurrentPlaylist = function(playlist) {
+        $scope.currentPlaylist = playlist;
+    };
+
+}
